ce7ab8bf816c0b68c25d737f32058a8d26e89e61
son san si <dangquocson1995@gmail.com>
2017-09-17 19:36:41 +0700

    iy

---
+++ Code/Xml/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}

35939605a05257133c8150d150b10292a93af97d
son san si <dangquocson1995@gmail.com>
2017-09-23 23:16:45 +0700

    a

---
+++ Code/Project_App/FreeHand/FreeHand/VoiceService.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace FreeHand
+{
+    [Service(Label = "VoiceService")]
+    [IntentFilter(new String[] { "com.yourname.VoiceService" })]
+    public class VoiceService : IntentService
+    {
+        IBinder binder;
+
+        protected override void OnHandleIntent(Intent intent)
+        {
+            // Perform your service logic here
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            binder = new VoiceServiceBinder(this);
+            return binder;
+        }
+    }
+
+    public class VoiceServiceBinder : Binder
+    {
+        readonly VoiceService service;
+
+        public VoiceServiceBinder(VoiceService service)
+        {
+            this.service = service;
+        }
+
+        public VoiceService GetVoiceService()
+        {
+            return service;
+        }
+    }
+}
